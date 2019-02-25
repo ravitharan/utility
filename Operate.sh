59e398bbb2a051824e0b5cd525e75615a72ed5d8
Ravi Ravitharan <gravitharan@miurasystems.com>
2019-02-25 12:21:53 +0000

    Operate.sh: Make use of xargs -I

--- Operate.sh
+++ Operate.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-while read ALine
-do
-  $* $ALine
-done
+
+# Example usage within vim
+# %! Operate.sh ls 
+# %! Operate.sh find /usr/include/ -path "*/@@" 
+
+cmd=$*
+if ! echo ${cmd} | grep -q "@@" ; then
+  cmd=${cmd}" @@"
+fi
+
+cat /dev/stdin | xargs -I@@ bash -c "${cmd}"
