384bf0db8f0100fc14393db8f59b453c2670af34
Hanusha Ravitharan <hanushat@gmail.com>
2021-10-08 21:07:08 +0100

    Fix HOME paths for windows

--- apply_dotfiles.py
+++ apply_dotfiles.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
 import os
+import sys
 import subprocess
 
-home_path = os.path.expanduser("~")
+utility_gitconfig = os.path.join(os.path.expanduser("~"), "utility", "gitconfig")
 
 link_dotfiles = [ "vimrc", "inputrc", "screenrc" ]
 
@@ -13,13 +14,13 @@ include_message = [
 f"""
 
 [include]
-    path = {home_path}/utility/gitconfig
+    path = {utility_gitconfig.encode('unicode_escape').decode()}
 
 """,
 f"""
 
-if [ -f {home_path}/utility/bashrc ]; then
-    source {home_path}/utility/bashrc
+if [ -f ~/utility/bashrc ]; then
+    source ~/utility/bashrc
 fi
 
 """
@@ -38,14 +39,13 @@ def issue_command(command):
     else: 
         return (True, cp.stdout)
 
-def utility_dot_included(home, dotfile):
-    utility_dotfile = f"{home}/utility/{dotfile}"
-    dst = f'{home}/.{dotfile}'
+def utility_dot_included(dotfile):
+    dst = os.path.join(os.path.expanduser('~'), '.' + dotfile)
     included = False
     try:
         with open(dst, "r") as file_in:
             for line in file_in:
-                if utility_dotfile in line:
+                if 'utility' in line and dotfile in line:
                     included = True
                     break
     except FileNotFoundError:
@@ -56,30 +56,31 @@ def utility_dot_included(home, dotfile):
 
 
 if __name__ == "__main__":
-    if os.geteuid() == 0:
-       print("ERROR: This script cannot be run as root")
-       exit(1)
+    if sys.platform == 'linux':
+        if os.geteuid() == 0:
+           print("ERROR: This script cannot be run as root")
+           exit(1)
 
 
     for dotfile in link_dotfiles:
-        dst = f'{home_path}/.{dotfile}'
+        dst = os.path.join(os.path.expanduser('~'), '.' + dotfile)
         if not os.path.isfile(dst):
-            issue_command(['ln', '-s', f'{home_path}/utility/{dotfile}', dst])
+            issue_command(['ln', '-s', f'~/utility/{dotfile}', dst])
         else:
             print(f"file {dst} already exist")
 
 
     for i in range(len(include_dotfile)):
-        dst = f'{home_path}/.{include_dotfile[i]}'
-        if not utility_dot_included(home_path, include_dotfile[i]):
+        dst = os.path.join(os.path.expanduser('~'), '.' + include_dotfile[i])
+        if not utility_dot_included(include_dotfile[i]):
             with open(dst, "a") as file_in:
                 file_in.write(include_message[i])
         else:
             print(f"file {dst} already included")
 
 
-    issue_command(['mkdir', '-p', f'{home_path}/.vim/colors'])
-    (ret, msg) = issue_command(['cp', '-vu', 'greens.vim', f'{home_path}/.vim/colors/'])
+    issue_command(['mkdir', '-p', f'~/.vim/colors'])
+    (ret, msg) = issue_command(['cp', '-vu', 'greens.vim', f'~/.vim/colors/'])
     print(msg)
 
 
