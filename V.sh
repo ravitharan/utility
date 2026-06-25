4c3916c21997b5335ec53787a06f997d84273940
Ravi G Ravitharan <ravitharan@gmail.com>
2026-06-25 16:32:11 +0100

    V.sh: Call nvim insted of vim

--- V.sh
+++ V.sh
@@ -4,7 +4,7 @@ if [ $# == 1 ]; then
   TITLE=$( echo ${TAG_FILE} | sed 's/^tags_\(.*\)$/\1/g' )
   LIST_FILE="files_"${TITLE}".txt"
   echo -e '\033k'${TITLE}'\033\\'
-  vim -c "set tag=${TAG_FILE}" -c "let g:FileName=\"${LIST_FILE}\""
+  nvim -c "set tag=${TAG_FILE}" -c "let g:FileName=\"${LIST_FILE}\""
 else
   echo "Argument error"
   echo "Usage: $0 <tag_file>"
